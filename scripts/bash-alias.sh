ce7035b6f72c6e1f65d4635ec7877da44f54e225
Brian Leonardo de Morais <morais.brian360@gmail.com>
2023-12-31 16:36:42 -0300

    Removido diretórios de scripts

--- scripts/bash-alias.sh
+++
@@ -1,3 +0,0 @@
-alias kubectl-ns='kubectl config set-context $(kubectl config current-context) --namespace'
-
-alias docker-clean='containers="$(docker ps -aq)" && if [ -n "$containers" ]; then docker stop $containers && docker rm $containers; fi && images="$(docker images -aq)" && if [ -n "$images" ]; then docker rmi $images; fi && networks="$(docker network ls -q)" && if [ -n "$networks" ]; then docker network prune -f; fi && volumes="$(docker volume ls -q)" && if [ -n "$volumes" ]; then docker volume prune -f; fi'
